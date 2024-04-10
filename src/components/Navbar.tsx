70abb94a353654425ce831af445de1ac80dcb381
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-10 11:20:01 +0800

    Added logo to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,22 @@
 // NEXT IMPORTS
+import Image from "next/image"
 import Link from 'next/link'
 
 const Navbar = (): JSX.Element => {
   return (
-    <nav className="flex justify-between w-full bg-red-400 p-6 md:px-10 lg:px-20">
-      <div>Logo</div>
-      <ul className="flex gap-3">
+    <nav className="flex justify-between items-center w-full p-4 md:px-10 lg:px-20">
+      <div className="flex items-end">
+      <Image
+        src="/logo.svg"
+        alt="Vercel Logo"
+        className="pr-2"
+        width={60}
+        height={60}
+        priority
+      />
+      <p className="font-bold text-4xl">Event'EXPO</p>
+      </div>
+      <ul className="flex gap-6">
         <li><Link href="/events">Events</Link></li>
         <li><Link href="/admin/events">Dashboard</Link></li>
       </ul>
