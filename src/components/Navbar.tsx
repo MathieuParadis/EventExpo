c192ea679faa44af4b00dcfac01a1f39f5b42237
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-10 11:07:04 +0800

    Added logo to public folder

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Navbar = (): JSX.Element => {
   return (
     <nav className="flex justify-between w-full bg-red-400 p-6 md:px-10 lg:px-20">
-      <div>Logo here</div>
+      <div>Logo</div>
       <ul className="flex gap-3">
         <li><Link href="/events">Events</Link></li>
         <li><Link href="/admin/events">Dashboard</Link></li>
