5e6f96694acd8775a587f0595de508b2b8665637
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-11 16:57:59 +0800

    edited card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -13,9 +13,10 @@ import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutli
 
 interface Props {
   event: Event
+  isAdmin?: boolean
 }
 
-const EventCard = ({ event }: Props): JSX.Element => {
+const EventCard = ({ event, isAdmin = false }: Props): JSX.Element => {
   return (
     <div key={event.id} className="flex sm:flex-col border-2 rounded-lg overflow-hidden h-[380px]">
       <Image
@@ -31,7 +32,7 @@ const EventCard = ({ event }: Props): JSX.Element => {
         <p className='text-xl font-semibold text-sky-700'>{event.title}</p>
         <div className="flex items-center gap-2 text-gray-500">
           <CalendarTodayOutlinedIcon fontSize='small' color="inherit" />
-          <p className='text-base'>{moment(event.startTime).format("ddd, MMM DD · hh:mm A")}</p>
+          <p className='text-base'>{moment(event.startTime).format('ddd, MMM DD · hh:mm A')}</p>
         </div>
         <div className="flex items-center gap-2 text-gray-500">
           <CheckCircleOutlineOutlinedIcon fontSize='small' color="inherit" />
@@ -43,10 +44,16 @@ const EventCard = ({ event }: Props): JSX.Element => {
       <div className='mt-auto flex gap-2 p-2 pt-3'>
         <button
           className="border border-[var(--primary-color)] rounded-md bg-white hover:bg-[var(--primary-color)] text-[var(--primary-color)] hover:text-white p-2 w-1/2"
-          >I'm interested</button>
-        <button
-          className="border rounded-md bg-[var(--primary-color)] hover:bg-[var(--primary-color-darker)] text-white p-2 w-1/2"
         >Learn more</button>
+        {isAdmin ? (
+          <button
+            className="border rounded-md bg-[var(--primary-color)] hover:bg-[var(--primary-color-darker)] text-white p-2 w-1/2"
+          >More actions</button>
+        ) : (
+          <button
+            className="border rounded-md bg-[var(--primary-color)] hover:bg-[var(--primary-color-darker)] text-white p-2 w-1/2"
+          >I&#39;m interested</button>
+        )}
       </div>
     </div>
   )
