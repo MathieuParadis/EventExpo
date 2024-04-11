c712c9c333fdf12adb376882a4c465524b51025d
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-11 18:47:31 +0800

    responsivness of eventCard

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -8,8 +8,8 @@ export default async function EventsPage() {
   const events = await fetchEvents()
 
   return (
-    <main className="bg-white grow p-4 md:px-10 lg:px-20">
-      <div className="grid grid-cols-4 gap-10">
+    <main className="bg-white grow p-4 md:px-10 lg:px-16">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
         {events != null && events.map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
