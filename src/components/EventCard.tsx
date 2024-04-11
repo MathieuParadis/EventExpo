b75542398f073158c59cc1e92d5a924a80202df7
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-11 15:06:35 +0800

    Edited card

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,3 +1,6 @@
+// NEXT IMPORTS
+import Image from 'next/image'
+
 // PRISMA IMPORTS
 import type { Event } from '@prisma/client'
 
@@ -7,9 +10,18 @@ interface Props {
 
 const EventCard = ({ event }: Props): JSX.Element => {
   return (
-    <div key={event.id} className="flex gap-2">
-      <p>{event.title}</p>
-      <p>{new Date(event.startTime).toISOString()}</p>
+    <div key={event.id} className="flex sm:flex-col gap-2 border-2 border-[var(--primary-color)] rounded-lg overflow-hidden h-[350px]">
+      <Image
+        src={event.image ?? "/defaultEvent.png"}
+        alt="Vercel Logo"
+        className="w-full h-[60%] object-cover"
+        width={450}
+        height={450}
+      />
+      <div>
+        <p>{event.title}</p>
+        <p>{new Date(event.startTime).toISOString()}</p>
+      </div>
     </div>
   )
 }
