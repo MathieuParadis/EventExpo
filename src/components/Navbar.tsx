112c0e9413d9821009b6107cb11d828944c31467
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-11 10:37:53 +0800

    working on mobile navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,32 +9,37 @@ const Navbar = (): JSX.Element => {
   const pathname = usePathname()
 
   return (
-    <nav className="flex justify-between items-center w-full p-4 md:px-10 lg:px-20 bg-black text-white">
-      <div className="flex items-center cursor-default">
-        <Image
-          src="/logo.svg"
-          alt="Vercel Logo"
-          className="pr-2"
-          width={45}
-          height={45}
-          priority
-        />
-        <p className="font-bold text-3xl">Event&#39;EXPO</p>
+    <nav>
+      {/* Navbar */}
+      <div className="flex justify-between items-center w-full p-4 md:px-10 lg:px-20 bg-black text-white">
+        <div className="flex items-center cursor-default">
+          <Image
+            src="/logo.svg"
+            alt="Vercel Logo"
+            className="pr-2"
+            width={45}
+            height={45}
+            priority
+          />
+          <p className="font-bold text-3xl">Event&#39;EXPO</p>
+        </div>
+        <ul className="hidden md:flex gap-6">
+          <li className="hover:scale-110">
+            <Link
+              className={`font-bold text-xl ${pathname === '/events' ? 'text-[var(--primary-color)]' : ''}`}
+              href="/events">Events
+            </Link>
+          </li>
+          <li className="hover:scale-110">
+            <Link
+              className={`font-bold text-xl ${pathname === '/admin/events' ? 'text-[var(--primary-color)]' : ''}`}
+              href="/admin/events">Dashboard
+            </Link>
+          </li>
+        </ul>
       </div>
-      <ul className="flex gap-6">
-        <li className="hover:scale-110">
-          <Link
-            className={`font-bold text-xl ${pathname === '/events' ? 'text-[var(--primary-color)]' : ''}`}
-            href="/events">Events
-          </Link>
-        </li>
-        <li className="hover:scale-110">
-          <Link
-            className={`font-bold text-xl ${pathname === '/admin/events' ? 'text-[var(--primary-color)]' : ''}`}
-            href="/admin/events">Dashboard
-          </Link>
-        </li>
-      </ul>
+
+      {/* Mobile Navigation */}
     </nav>
   )
 }
