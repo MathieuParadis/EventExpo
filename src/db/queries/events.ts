ced8dd54a8aefb26d6dd43012d88a0742f17dfe7
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-12 17:06:33 +0800

    edit default event image, added border at the bottom of image and hidding past Events

--- src/db/queries/events.ts
+++ src/db/queries/events.ts
@@ -6,6 +6,13 @@ import { db } from '@/db'
 
 export async function fetchEvents(): Promise<Event[]> {  // Function to fetch all posts from the database.
   return await db.event.findMany({
+    where: {
+      startTime: {
+        // Filter events where the startTime is greater than or equal to the current date
+        // (i.e., today or in the future)
+        gte: new Date(),
+      },
+    },
     orderBy: [
       {
         startTime: 'asc',
