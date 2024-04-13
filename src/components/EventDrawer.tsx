99d242c8f798a5c99c3748eeb5d4773f73f20d55
MathieuParadis <76945713+MathieuParadis@users.noreply.github.com>
2024-04-14 00:15:04 +0800

    UI/event drawer (#50)

--- src/components/EventDrawer.tsx
+++ src/components/EventDrawer.tsx
@@ -3,12 +3,22 @@
 // REACT IMPORTS
 import { useEffect } from 'react'
 
+// MOMENT IMPORT
+import moment from 'moment'
+
 // MUI IMPORTS
+import { Divider } from '@mui/material'
 import Drawer from '@mui/material/Drawer'
 
 // HOOKS IMPORTS
 import { useAppSelector, useAppDispatch } from '@/lib/hooks'
 import { closeReadEventModal } from '@/lib/features/eventModals/eventModalsSlice'
+
+// ICONS IMPORTS
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined'
+import CheckCircleOutlineOutlinedIcon from '@mui/icons-material/CheckCircleOutlineOutlined'
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined'
+import FmdGoodOutlinedIcon from '@mui/icons-material/FmdGoodOutlined'
 
 const EventDrawer = (): JSX.Element => {
   const dispatch = useAppDispatch()
@@ -49,7 +59,37 @@ const EventDrawer = (): JSX.Element => {
       }}
     >
       {event != null && (
-        <div>content</div>
+        <div className="h-full flex flex-col gap-4 md:gap-5 lg:gap-6 p-4 md:p-5 lg:p-6">
+          {/* event title */}
+          <div className='flex justify-between items-center text-xl sm:text-2xl md:text-3xl lg:text-4xl line-clamp-2'>
+            <h2 className='text-[var(--primary-color)] font-bold'>{event.title}</h2>
+            <CloseOutlinedIcon className="cursor-pointer" fontSize="inherit" onClick={closeReadModal} />
+          </div>
+
+          <Divider />
+
+          {/* event details */}
+          <div className="flex flex-col sm:flex-row gap-4 md:gap-5 lg:gap-6 grow">
+            <div className='w-full sm:w-2/3 flex flex-col gap-4 md:gap-5 lg:gap-6'>
+              <p className="text-base md:text-lg lg:text-xl">{event.description}</p>
+              <div className="flex gap-2">
+                <CalendarTodayOutlinedIcon fontSize="small" color="inherit" />
+                <p className="text-sm md:text-base">{moment(event.startTime).format('ddd, MMM DD · hh:mm A')}</p>
+              </div>
+              <div className="flex gap-2">
+                <FmdGoodOutlinedIcon fontSize="small" color="inherit" />
+                <p className="text-sm md:text-base">{event.location}</p>
+              </div>
+              <div className="flex gap-2">
+                <CheckCircleOutlineOutlinedIcon fontSize="small" color="inherit" />
+                <p className="text-sm md:text-base">{event.interestCount} people interested</p>
+              </div>
+            </div>
+            <Divider className="hidden sm:block" orientation="vertical" flexItem />
+            <Divider className="sm:hidden" />
+            <div className="grow">buttons</div>
+          </div>
+        </div>
       )}
     </Drawer>
   )
