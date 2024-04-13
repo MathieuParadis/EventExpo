6ed8ff286d69f0df8a31aa464290fd02c2a99ca4
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-13 23:53:57 +0800

    designed the content of the drawer

--- src/components/EventDrawer.tsx
+++ src/components/EventDrawer.tsx
@@ -4,11 +4,15 @@
 import { useEffect } from 'react'
 
 // MUI IMPORTS
+import { Divider } from '@mui/material'
 import Drawer from '@mui/material/Drawer'
 
 // HOOKS IMPORTS
 import { useAppSelector, useAppDispatch } from '@/lib/hooks'
 import { closeReadEventModal } from '@/lib/features/eventModals/eventModalsSlice'
+
+// ICONS IMPORTS
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined'
 
 const EventDrawer = (): JSX.Element => {
   const dispatch = useAppDispatch()
@@ -49,7 +53,19 @@ const EventDrawer = (): JSX.Element => {
       }}
     >
       {event != null && (
-        <div>content</div>
+        <div className="h-full flex flex-col gap-2 md:gap3 lg:gap4 p-2 md:p-3 lg:p-4">
+          <div className='flex justify-between items-center text-1xl sm:text-2xl md:text-3xl lg:text-4xl line-clamp-2'>
+            <h2 className='text-[var(--primary-color)] font-bold'>{event.title}</h2>
+            <CloseOutlinedIcon className="cursor-pointer" fontSize="inherit" onClick={closeReadModal} />
+          </div>
+          <Divider />
+          <div className="flex flex-col md:flex-row gap-2 md:gap3 lg:gap4 grow">
+            <div className='w-2/3'>content</div>
+            <Divider className='hidden md:block' orientation="vertical" flexItem />
+            <Divider className='md:hidden' />
+            <div>buttons</div>
+          </div>
+        </div>
       )}
     </Drawer>
   )
