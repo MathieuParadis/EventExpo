a389f88e6929ca04aae4b779a5abc85c0f7c142b
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-13 22:10:27 +0800

    eventDrawer working

--- src/components/EventDrawer.tsx
+++ src/components/EventDrawer.tsx
@@ -1,27 +1,29 @@
+'use client'
 
 // REACT IMPORTS
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 // MUI IMPORTS
 import Drawer from '@mui/material/Drawer'
-import Button from '@mui/material/Button'
 
-const EventDrawerBtn = (): JSX.Element => {
-  const [isOpen, setIsOpen] = useState(false)
+// HOOKS IMPORTS
+import { useAppSelector, useAppDispatch } from '@/lib/hooks'
+import { closeReadEventModal } from '@/lib/features/eventModals/eventModalsSlice'
 
-  const openDrawer = (): void => {
-    setIsOpen(true)
+const EventDrawer = (): JSX.Element => {
+  const dispatch = useAppDispatch()
+  const eventModals = useAppSelector((state) => state.modals)
+  const { isReadEvent, event } = eventModals
+
+  const closeReadModal = ():void => {
+    dispatch(closeReadEventModal())
   }
 
-  const closeDrawer = (): void => {
-    setIsOpen(false)
-  }
-
-  // Close drawer on Escape
+  // Close modal on Escape
   useEffect((): (() => void) => {
     const close = (e: KeyboardEvent): void => {
       if (e.key === 'Escape') {
-        closeDrawer()
+        closeReadModal()
       }
     }
 
@@ -32,34 +34,25 @@ const EventDrawerBtn = (): JSX.Element => {
   })
 
   return (
-    <>
-      <Button
-        className='focus:outline-none rounded bg-white hover:bg-[var(--primary-color)] text-[var(--primary-color)] hover:text-white p-1 w-1/2'
-        style={{ textTransform: 'capitalize', border: '1px solid var(--primary-color)' }}
-        onClick={openDrawer}
-      >
-        Learn more
-      </Button>
-      <Drawer
-        className='top-[var(--navbar-height)]'
-        anchor="right"
-        open={isOpen}
-        onClose={closeDrawer}
-        PaperProps={{
-          className: 'h-[calc(100%-var(--navbar-height))] top-[var(--navbar-height)] w-full sm:w-[100%] md:w-[90%] lg:w-[80%] xl:w-[72%] rounded-tl-md rounded-bl-md'
-        }}
-        slotProps={{
-          backdrop: {
-            sx: {top: 'var(--navbar-height)'}
-          }
-        }}
-      >
-        <div>
-          content
-        </div>
-      </Drawer>
-    </>
+    <Drawer
+      className='top-[var(--navbar-height)]'
+      anchor="right"
+      open={isReadEvent}
+      onClose={closeReadModal}
+      PaperProps={{
+        className: 'h-[calc(100%-var(--navbar-height))] top-[var(--navbar-height)] w-full sm:w-[100%] md:w-[90%] lg:w-[80%] xl:w-[72%] rounded-tl-md rounded-bl-md'
+      }}
+      slotProps={{
+        backdrop: {
+          sx: {top: 'var(--navbar-height)'}
+        }
+      }}
+    >
+      <div>
+      content
+      </div>
+    </Drawer>
   )
 }
 
-export default EventDrawerBtn
+export default EventDrawer
