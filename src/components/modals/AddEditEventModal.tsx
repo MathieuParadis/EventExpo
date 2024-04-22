0f4c6dd276f71281265867032bd17035cfc56742
MathieuParadis <mathieuyl.paradis@gmail.com>
2024-04-22 09:08:07 +0800

    edited modal

--- src/components/modals/AddEditEventModal.tsx
+++ src/components/modals/AddEditEventModal.tsx
@@ -42,37 +42,33 @@ const AddEditEventModal = (): JSX.Element => {
         <div className="absolute w-full h-full bg-black opacity-40"></div>
 
         {/* content */}
-        <div className="absolute flex flex-col justify-between gap-2 md:gap-3 lg:gap-4 w-[90%] md:w-[500px] aspect-[5/6] bg-white rounded-md p-2 md:p-4">
-          <div className="flex flex-col justify-center gap-2 md:gap-3 lg:gap-4">
+        <div className="absolute flex flex-col justify-between gap-2 md:gap-3 lg:gap-4 w-[90%] md:w-[500px] aspect-[5/6] bg-white rounded-md">
+          <div className="flex justify-center gap-2 md:gap-3 lg:gap-4 border-b p-2 md:p-4">
             <h2 className='text-xl sm:text-2xl md:text-3xl lg:text-4xl text-center font-semibold text-[var(--primary-color-darker)]'>
               {isAddEvent && <>New event</>}
               {isEditEvent && <>Edit event</>}
             </h2>
-            <Divider className='px-[-8px] md:px-[-16px]' />
           </div>
 
-          <div className="grow overflow-y-auto h-10">
+          <div className="grow overflow-y-auto h-10 p-2 md:p-4">
             <EventForm event={event ?? newEvent} />
           </div>
 
-          <div className="flex flex-col gap-2 md:gap-3 lg:gap-4">
-            <Divider />
-            <div className="flex justify-center gap-2 md:gap-3 lg:gap-4">
-              <Button
-                className="w-1/2 md:w-[150px] rounded focus:outline-none bg-gray-400 hover:bg-gray-500 text-white py-1 px-2"
-                style={{ textTransform: 'capitalize'}}
-                onClick={closeAddEditModal}
-              >
-              Cancel
-              </Button>
-              <Button
-                className="w-1/2 md:w-[150px] rounded focus:outline-none bg-green-500 hover:bg-green-600 text-white py-1 px-2"
-                style={{ textTransform: 'capitalize'}}
-                onClick={closeAddEditModal}
-              >
-              Save
-              </Button>
-            </div>
+          <div className="flex justify-center gap-2 md:gap-3 lg:gap-4 border-t p-2 md:p-4">
+            <Button
+              className="w-1/2 md:w-[150px] rounded focus:outline-none bg-gray-400 hover:bg-gray-500 text-white py-1 px-2"
+              style={{ textTransform: 'capitalize'}}
+              onClick={closeAddEditModal}
+            >
+            Cancel
+            </Button>
+            <Button
+              className="w-1/2 md:w-[150px] rounded focus:outline-none bg-green-500 hover:bg-green-600 text-white py-1 px-2"
+              style={{ textTransform: 'capitalize'}}
+              onClick={closeAddEditModal}
+            >
+            Save
+            </Button>
           </div>
         </div>
       </div>
